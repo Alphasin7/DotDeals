20bb023364353512bbee900a70bf3a0a7baf4ef6
Cypher <tangaraaymen@outlook.com>
2024-05-12 11:53:42 +0100

    Category_Table_Created

--- DotDeals/Data/ApplicationDbContext.cs
+++ DotDeals/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotDeals.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotDeals.Data
 {
@@ -8,5 +9,6 @@ namespace DotDeals.Data
         {
                 
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
