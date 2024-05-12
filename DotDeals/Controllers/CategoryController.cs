e9179b7865b42aab5ef632b3bfadf55065af730f
Cypher <tangaraaymen@outlook.com>
2024-05-12 12:45:49 +0100

    Get&Display_All_Categories

--- DotDeals/Controllers/CategoryController.cs
+++ DotDeals/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotDeals.Data;
+using DotDeals.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotDeals.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
