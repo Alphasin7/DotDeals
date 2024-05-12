19ec3941f46dbd6168d2f5ff0778895897214eb8
Cypher <tangaraaymen@outlook.com>
2024-05-12 12:24:17 +0100

    seedCategoryTable

--- DotDeals/Data/ApplicationDbContext.cs
+++ DotDeals/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace DotDeals.Data
                 
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Biography", DisplayOrder = 3 }
+                );
+        }
     }
 }
