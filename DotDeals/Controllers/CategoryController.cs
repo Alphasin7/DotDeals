39dbdeb2b9c85531852b33d3e452e60c3b1967a1
Cypher <tangaraaymen@outlook.com>
2024-05-13 14:06:46 +0100

    Get_Category_Details_to_EditV2

--- DotDeals/Controllers/CategoryController.cs
+++ DotDeals/Controllers/CategoryController.cs
@@ -46,8 +46,7 @@ namespace DotDeals.Controllers
                 return NotFound();
             }
             Category? categoryFromDb = _db.Categories.Find(id);
-            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            
             if (categoryFromDb == null)
             {
                 return NotFound();
